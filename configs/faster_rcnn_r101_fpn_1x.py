c56a120761082af3e3677d1661174188cb22d9ab
vishaksakthivel <52810559+vishaksakthivel@users.noreply.github.com>
2020-11-05 16:44:15 +0530

    Update faster_rcnn_r101_fpn_1x.py

--- configs/faster_rcnn_r101_fpn_1x.py
+++ configs/faster_rcnn_r101_fpn_1x.py
@@ -100,7 +100,7 @@ test_cfg = dict(
 )
 # dataset settings
 dataset_type = 'MyDataset'
-data_root = '/content/data/VOC2007/ImageSets/'
+data_root = '/content/data/VOC2007/ImageSets/Main/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
