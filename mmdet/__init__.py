28398a8ddf8a7a2baffd8b54a854e12a88da8642
vishaksakthivel <52810559+vishaksakthivel@users.noreply.github.com>
2020-11-05 14:52:39 +0530

    Update __init__.py

--- mmdet/__init__.py
+++ mmdet/__init__.py
@@ -1,3 +1,10 @@
-from .version import __version__, short_version
-
-__all__ = ['__version__', 'short_version']
+# ...
+from .wider_face import WIDERFaceDataset
+from .xml_style import XMLDataset
+### ADDING YOUR DATASET
+from .my_dataset import MyDataset
+__all__ = [
+    'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset',
+    'CityscapesDataset', 'GroupSampler', 'DistributedGroupSampler',
+    'build_dataloader', 'ConcatDataset', 'RepeatDataset', 'WIDERFaceDataset',
+    'DATASETS', 'build_dataset','MyDataset','table','my_dataset'] # Also Add in List
