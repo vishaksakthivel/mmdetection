7f6f944b335a11990199231ff0066f4af3d1f58f
vishaksakthivel <52810559+vishaksakthivel@users.noreply.github.com>
2020-11-11 00:39:37 +0530

    Update faster_rcnn_r50_fpn_1x_voc0712.py

--- configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py
+++ configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py
@@ -136,10 +136,9 @@ data = dict(
         dataset=dict(
             type=dataset_type,
             ann_file=[
-                data_root + 'VOC2007/ImageSets/Main/trainval.txt',
-                data_root + 'VOC2012/ImageSets/Main/trainval.txt'
+                data_root + 'VOC2007/ImageSets/Main/trainval.txt'
             ],
-            img_prefix=[data_root + 'VOC2007/', data_root + 'VOC2012/'],
+            img_prefix=[data_root + 'VOC2007/'],
             pipeline=train_pipeline)),
     val=dict(
         type=dataset_type,
